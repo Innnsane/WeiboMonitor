14c6ea0075822b84223b3f052add4d297ed75ef9
StarHeartHunt <starheart233@gmail.com>
2021-05-18 12:18:48 +0800

    feat(timetrans): transform utc offset

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ import time
 
 
 def getUnixFromJS(time_str):
-    struct_t = time.strptime(time_str, "%a %b %d %H:%M:%S +0800 %Y")
+    struct_t = time.strptime(time_str, "%a %b %d %H:%M:%S %z %Y")
 
     return int(time.mktime(struct_t))
